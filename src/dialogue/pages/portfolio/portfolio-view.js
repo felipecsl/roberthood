206243335e086e58c3d43ceb3b7275754833efc3
Felipe Lima <felipe.lima@gmail.com>
2016-03-13 20:57:21 -0700

    Adds colors to quotes

--- src/dialogue/pages/portfolio/portfolio-view.js
+++ src/dialogue/pages/portfolio/portfolio-view.js
@@ -1,8 +1,20 @@
 import {div, p, h, h1, ul, li} from '@cycle/dom'
 import {formatMoney} from 'accounting'
 
+/**
+ * Formats the number of shares amount as an Integer. Eg.: "2.000" -> "2"
+ */
 function formatShares(amount) {
   return amount.substring(0, amount.indexOf("."))
+}
+
+/**
+ * Returns the CSS class name to be used for displaying the provided quote based on whether
+ * it is going up or down
+ */
+function quoteClass(quote) {
+  return parseFloat(quote.last_trade_price) > parseFloat(quote.previous_close)
+    ? ".quote-up" : ".quote-down"
 }
 
 const view = state$ => {
@@ -20,19 +32,23 @@ const view = state$ => {
         div([
           h('paper-card', { heading: 'Portfolio' }, [
             h('.card-content', [
-              h1(formatMoney(s.portfolio.last_core_equity))
-            ]),
-            div('.portfolio-items', { attributes: { role: 'listbox' }}, positions.map(position =>
-              h('paper-item', [
-                h('paper-item-body', { attributes: { 'two-line': '' }}, [
-                  div([
-                    `${position.instrument.symbol}`,
-                    div('.right', formatMoney(position.instrument.quote.last_trade_price))
-                  ]),
-                  div({ attributes: { secondary: '' }}, `${formatShares(position.quantity)} Shares`)
+              h1('.center', formatMoney(s.portfolio.last_core_equity)),
+              div('.portfolio-items', { attributes: { role: 'listbox' }}, positions.map(position =>
+                h('paper-item', [
+                  h('paper-item-body', { attributes: { 'two-line': '' }}, [
+                    div([
+                      `${position.instrument.symbol}`,
+                      div(`.right .${quoteClass(position.instrument.quote)}`, [
+                        formatMoney(position.instrument.quote.last_trade_price)
+                      ])
+                    ]),
+                    div({ attributes: { secondary: '' }}, [
+                      `${formatShares(position.quantity)} Shares`
+                    ])
+                  ])
                 ])
-              ])
-            ))
+              ))
+            ])
           ])
         ])
       ])
