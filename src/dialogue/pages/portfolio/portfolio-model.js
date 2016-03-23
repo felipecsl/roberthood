b82fd5e5f43d6a529bcef0ae4acd29e56957f187
Felipe Lima <felipe.lima@gmail.com>
2016-03-22 22:39:23 -0700

    DRY up responses

--- src/dialogue/pages/portfolio/portfolio-model.js
+++ src/dialogue/pages/portfolio/portfolio-model.js
@@ -1,68 +1,15 @@
 import {Observable} from 'rx'
-
-function instrumentIdFromPosition(position) {
-  return position.instrument
-    .replace("https://api.robinhood.com/instruments/", "")
-    .replace("/", "")
-}
-
-function filterRequestByCategory(request$, category) {
-  return request$.flatMap(x => x)
-    .filter(res => res.request.category === category)
-}
+import responses from './responses'
 
 const portfolioModel = (request$, state$) => {
-  const user$ = filterRequestByCategory(request$, 'user')
-      .map(res => res.body)
-      .flatMap(res => state$.take(1).map((state) => {
-        state.user = res
-        return state
-      }))
-  const accounts$ = filterRequestByCategory(request$, 'account')
-      .map(res => res.body.results[0])
-      .flatMap(res => state$.take(1).map((state) => {
-        state.account = res
-        return state
-      }))
-  const portfolio$ = filterRequestByCategory(request$, 'portfolio')
-      .map(res => res.body)
-      .flatMap(res => state$.take(1).map((state) => {
-        state.portfolio = res
-        return state
-      }))
-  const historicals$ = filterRequestByCategory(request$, 'historicals')
-      .map(res => res.body.equity_historicals)
-      .flatMap(res => state$.take(1).map((state) => {
-        state.historicals = res
-        return state
-      }))
-  const positions$ = filterRequestByCategory(request$, 'positions')
-      .map(res => res.body.results)
-      .map(results => results.map(pos => {
-        pos.instrumentId = instrumentIdFromPosition(pos)
-        return pos
-      }))
-      .flatMap(res => state$.take(1).map((state) => {
-        state.positions = res
-        return state
-      }))
-  const instruments$ = filterRequestByCategory(request$, 'instruments')
-      .flatMap(res => state$.take(1).map((state) => {
-        let position = state.positions
-          .filter(p => p.instrumentId === res.request.instrumentId)
-        position[0].instrument = res.body
-        return state
-      }))
-  const quotes$ = filterRequestByCategory(request$, 'quotes')
-      .map(res => res.body.results)
-      .flatMap(res => state$.take(1).map((state) => {
-        res.forEach(quote => {
-          let position = state.positions
-            .filter(p => p.instrument.symbol === quote.symbol)
-          position[0].instrument.quote = quote
-        })
-        return state
-      }))
+  const user$ = responses.user$(request$, state$)
+  const accounts$ = responses.accounts$(request$, state$)
+  const portfolio$ = responses.portfolio$(request$, state$)
+  const historicals$ = responses.historicals$(request$, state$)
+  const positions$ = responses.positions$(request$, state$)
+  const instruments$ = responses.instruments$(request$, state$)
+  const quotes$ = responses.quotes$(request$, state$)
+
   return Observable.merge(state$.take(1), user$, accounts$, portfolio$,
     historicals$, positions$, instruments$, quotes$)
 }
---
+++ src/dialogue/pages/portfolio/responses.js
@@ -0,0 +1,71 @@
+function instrumentIdFromPosition(position) {
+  return position.instrument
+    .replace("https://api.robinhood.com/instruments/", "")
+    .replace("/", "")
+}
+
+function filterByCategory(request$, category) {
+  return request$.flatMap(x => x)
+    .filter(res => res.request.category === category)
+}
+
+const user$ = (request$, state$) => filterByCategory(request$, 'user')
+  .map(res => res.body)
+  .flatMap(res => state$.take(1).map((state) => {
+    state.user = res
+    return state
+  }))
+
+const accounts$ = (request$, state$) => filterByCategory(request$, 'account')
+  .map(res => res.body.results[0])
+  .flatMap(res => state$.take(1).map((state) => {
+    state.account = res
+    return state
+  }))
+
+const portfolio$ = (request$, state$) => filterByCategory(request$, 'portfolio')
+  .map(res => res.body)
+  .flatMap(res => state$.take(1).map((state) => {
+    state.portfolio = res
+    return state
+  }))
+
+const historicals$ = (request$, state$) => filterByCategory(request$, 'historicals')
+  .map(res => res.body.equity_historicals)
+  .flatMap(res => state$.take(1).map((state) => {
+    state.historicals = res
+    return state
+  }))
+
+const positions$ = (request$, state$) => filterByCategory(request$, 'positions')
+  .map(res => res.body.results)
+  .map(results => results.map(pos => {
+    pos.instrumentId = instrumentIdFromPosition(pos)
+    return pos
+  }))
+  .flatMap(res => state$.take(1).map((state) => {
+    state.positions = res
+    return state
+  }))
+
+const instruments$ = (request$, state$) => filterByCategory(request$, 'instruments')
+  .flatMap(res => state$.take(1).map((state) => {
+    let position = state.positions
+      .filter(p => p.instrumentId === res.request.instrumentId)
+    position[0].instrument = res.body
+    return state
+  }))
+
+const quotes$ = (request$, state$) => filterByCategory(request$, 'quotes')
+  .map(res => res.body.results)
+  .flatMap(res => state$.take(1).map((state) => {
+    res.forEach(quote => {
+      let position = state.positions
+        .filter(p => p.instrument.symbol === quote.symbol)
+      position[0].instrument.quote = quote
+    })
+    return state
+  }))
+
+export default {user$, accounts$, portfolio$, historicals$, positions$,
+  instruments$, quotes$}
