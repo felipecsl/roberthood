585690eaa7787b800e91636caa1a7de869738f3f
Felipe Lima <felipe.lima@gmail.com>
2016-03-31 00:11:41 -0700

    Dummy commit

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ import Pol from './polymer'
 // This is the Cycle run. first argument is our mainApp then an object:
 // DOM is the ID or class we want the cycle to render onto our page.
 // History is using our makeHistoryDriver to deal with routing.
+
 console.log("Initializing Cycle...")
 
 const sources = {
