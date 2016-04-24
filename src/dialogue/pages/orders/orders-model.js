6ed30e492d24048ce69af1f582ef58bd05e80ca4
Felipe Lima <felipe.lima@gmail.com>
2016-04-23 22:07:06 -0700

    Fixes order history symbols

--- src/dialogue/pages/orders/orders-model.js
+++ src/dialogue/pages/orders/orders-model.js
@@ -1,9 +1,19 @@
 import {Observable} from 'rx'
 
-export default (state$, response$$) => Observable.merge(state$.take(1), response$$.flatMap(x => x)
-  .filter(res => res.request.category === 'orders')
-  .map(res => res.body.results)
-  .flatMap(res => state$.take(1).map((state) => {
-    state.orders = res
-    return state
-  })))
+export default (state$, response$$) => Observable.merge(
+  state$.take(1),
+  response$$.flatMap(x => x)
+    .filter(res => res.request.category === 'orders')
+    .map(res => res.body.results)
+    .flatMap(res => state$.take(1).map((state) => {
+      state.orders = res
+      return state
+    })),
+  response$$.flatMap(x => x)
+    .filter(res => res.request.category === 'orderInstrument')
+    .flatMap(res => state$.take(1)
+      .map(state => {
+        const order = state.orders.find(o => o.id === res.request.orderId)
+        order.instrument = res.body
+        return state
+      })))
--- src/dialogue/pages/orders/orders-view.js
+++ src/dialogue/pages/orders/orders-view.js
@@ -3,20 +3,20 @@ import {div, p, h, h1, ul, li, a, form, hr} from '@cycle/dom'
 import {formatMoney, toFixed} from 'accounting'
 import helpers from '../../../helpers'
 
-export default (state$) => state$.filter(m => m.orders !== undefined)
+export default (state$) => state$
+  .filter(m => m.orders !== undefined && m.orders.every(o => typeof o.instrument === 'object'))
   .map(s => div([
     h(`paper-card`, { heading: 'Orders' }, [
       h(`.card-content.orders`, [
         h(`paper-listbox`, { attributes: { role: 'listbox' }}, s.orders.map(o => h('paper-item', [
           h('paper-item-body', { attributes: { 'two-line': '' }}, [
             div([
-              o.state,
+              o.instrument.symbol,
               div(`.right`, `${o.type} ${o.side}`),
             ]),
             div({ attributes: { secondary: '' }}, [
               div([
-                s.positions.find(p =>
-                  p.instrumentId === helpers.instrumentIdFromUrl(o.instrument)).symbol,
+                o.state,
                 div(`.right`, `${formatMoney(parseInt(o.quantity) * o.average_price)}`),
               ]),
             ])
